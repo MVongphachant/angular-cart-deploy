994ad38856d5c6187b4a39852cb30003ec6b7691
Mario <m.vongphachant@live.com>
2021-02-25 22:48:34 -0500

    added button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from '@angular/common/http';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
+  isRed = false;
+
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     return this.http.get('http://localhost:3000/cart-items').subscribe(data => console.log(data))
+  }
+
+  onClick() {
+    this.isRed = !this.isRed;
   }
 
 }
